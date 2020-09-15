7e441b0ddaca86b64651fd4196e7f9950dc1b754
cleminator <alvinclemens@gmail.com>
2020-09-15 14:16:51 -0600

    little update

--- assets/portfswiper.js
+++ assets/portfswiper.js
@@ -1,17 +1,3 @@
-var galleryThumbs = new Swiper('.gallery-thumbs', {
-    spaceBetween: 10,
-    slidesPerView: 4,
-    freeMode: true,
-    watchSlidesVisibility: true,
-    watchSlidesProgress: true,
-  });
-  var galleryTop = new Swiper('.gallery-top', {
-    spaceBetween: 5,
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-    thumbs: {
-      swiper: galleryThumbs
-    }
+document.getElementById("subBTN").addEventListener("click", function(event){
+  event.preventDefault()
   });
